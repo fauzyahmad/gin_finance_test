b2d71fcf81d4bb91d9fb2f5d377c2056184254dd
fauzyahmad <ahmadfauzycg@gmail.com>
2021-07-09 17:09:20 +0800

    password with textfield custom widget

--- lib/ui/views/create_password_screen.dart
+++ lib/ui/views/create_password_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gin_finance_test/ui/widgets/custom_textfield.dart';
 
 class CreatePasswordScreen extends StatefulWidget {
   CreatePasswordScreen({Key? key}) : super(key: key);
@@ -8,6 +9,7 @@ class CreatePasswordScreen extends StatefulWidget {
 }
 
 class _CreatePasswordScreenState extends State<CreatePasswordScreen> {
+  TextEditingController _password = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,10 +27,72 @@ class _CreatePasswordScreenState extends State<CreatePasswordScreen> {
             Text(
               'Password will be used to login to account',
             ),
-            //textformfield
+            CustomTextField(
+              hintText: 'Create Password',
+              obscureText: true,
+              onSubmitField: () => print('aa'),
+              onChange: () => print('qwe'),
+              controller: _password,
+              prefixIcon: Icon(Icons.remove_red_eye),
+            ),
             Text('Complexity'),
             //Row for uppercase, lowecase, number, characters
             //TODO: make algoritm check if all required passed
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('a'),
+                      Text('Lowercase'),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('A'),
+                      Text('Uppercase'),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('123'),
+                      Text('Number'),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('9+'),
+                      Text('Characters'),
+                    ],
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
