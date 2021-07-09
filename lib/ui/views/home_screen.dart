4a7064c8ec2b07afdae1fde26cc77e0153f284de
fauzyahmad <ahmadfauzycg@gmail.com>
2021-07-09 09:27:36 +0800

    homescreen page added

---
+++ lib/ui/views/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = '/homepage';
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Welcome to',
+            ),
+            Text('GIN Finance'),
+            Text('Welcome to The Bank of The Future. '
+                ''
+                'Manage and Track your accounts on the go.'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EmailCheck extends StatefulWidget {
+  EmailCheck({Key? key}) : super(key: key);
+
+  @override
+  _EmailCheckState createState() => _EmailCheckState();
+}
+
+class _EmailCheckState extends State<EmailCheck> {
+  TextEditingController _email = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(12),
+        ),
+      ),
+      padding: EdgeInsets.all(5),
+      child: null,
+      //textformfield
+    );
+  }
+}
